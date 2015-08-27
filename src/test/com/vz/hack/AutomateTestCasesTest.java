515020ba8c37468504b9cc431e1a7f388649f94e
madhanvz1991 <madhanswamy17@gmail.com>
2015-08-27 17:55:28 +0530

    Update and rename AutomateTestIT.java to AutomateTestCasesTest.java

---
+++ src/test/com/vz/hack/AutomateTestCasesTest.java
@@ -0,0 +1,60 @@
+package com.vz.hack;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class AutomateTestCasesTest {
+	
+		
+	public AutomateTestCasesTest() {
+	    }
+	    
+	    @BeforeClass
+	    public static void setUpClass() {
+	    }
+	    
+	    @AfterClass
+	    public static void tearDownClass() {
+	    }
+	    
+	    @Before
+	    public void setUp() {
+	    }
+	    
+	    @After
+	    public void tearDown() {
+	    }
+
+	    
+	    @Test
+	    public void successTestOpr() {
+	        System.out.println("testOpr");
+	        int a = 5;
+	        int b = 5;
+	        AutomateTestCases instance = new AutomateTestCases();
+	        int expResult = 10;
+	        int result = instance.testOpr(a, b);
+	        assertEquals(expResult, result);
+	    }
+	    
+	    @Test
+	    public void failureTestOpr() {
+	        System.out.println("testOpr");
+	        int a = 10;
+	        int b = 5;
+	        AutomateTestCases instance = new AutomateTestCases();
+	        int expResult = 10;
+	        int result = instance.testOpr(a, b);
+	        assertEquals(expResult, result);
+	    }
+	    
+	}
+
+
+
