cab20f2af9114bcdfcf50762ef0919fcd84b7b5d
madhanvz1991 <madhanswamy17@gmail.com>
2015-10-09 20:56:41 +0530

    Create ArithmeticCalculation.java

---
+++ src/main/java/com/vz/hack/ArithmeticCalculation.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.List;
+import java.util.Map;
+
+public class ArithmeticCalculation {
+
+	/*public int oprAdd(int a, int b){
+        return a + b;
+    }
+	
+	public long oprSub(long a, long b){
+        return a - b;
+    }
+    
+    public double oprMul(int a, int b){
+        return a * b;
+    }
+    
+    public double oprMul2(double a, double b){
+        return a * b;
+    }*/
+    
+   /* public Map<String,String> stringTest(Map<String,String> x, List y){
+        return x;
+    }*/
+    
+    public List<Map> listTest(List<String> a, List<Map> b){
+        return b;
+    }
+	
+}
